1d219a19b15218915f95039698e5accbfa532c2d
@a <28899947+astinz@users.noreply.github.com>
2023-08-27 19:05:39 +0300

    add ios support...

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -41,6 +41,9 @@ kotlin {
     testRuns["test"].executionTask.configure { useJUnitPlatform() }
   }
   android { publishLibraryVariants("release", "debug") }
+  listOf(iosX64(), iosArm64(), iosSimulatorArm64()).forEach {
+    it.binaries.framework { baseName = "commonMain" }
+  }
   js(IR) { browser { commonWebpackConfig { cssSupport { enabled.set(true) } } } }
   val hostOs = System.getProperty("os.name")
   val isMingwX64 = hostOs.startsWith("Windows")
@@ -68,6 +71,25 @@ kotlin {
     }
     val androidMain by getting {
       dependencies { implementation("io.ktor:ktor-client-okhttp:$ktorVersion") }
+    }
+    val iosX64Main by getting
+    val iosArm64Main by getting
+    val iosSimulatorArm64Main by getting
+    val iosMain by creating {
+      dependsOn(commonMain)
+      iosX64Main.dependsOn(this)
+      iosArm64Main.dependsOn(this)
+      iosSimulatorArm64Main.dependsOn(this)
+      dependencies { implementation("io.ktor:ktor-client-darwin:$ktorVersion") }
+    }
+    val iosX64Test by getting
+    val iosArm64Test by getting
+    val iosSimulatorArm64Test by getting
+    val iosTest by creating {
+      dependsOn(commonTest)
+      iosX64Test.dependsOn(this)
+      iosArm64Test.dependsOn(this)
+      iosSimulatorArm64Test.dependsOn(this)
     }
     val jvmMain by getting {
       dependencies { implementation("io.ktor:ktor-client-cio:$ktorVersion") }
