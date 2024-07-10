9d5ebc756996ec9e585610313b056a545fb002e6
astinz <28899947+astinz@users.noreply.github.com>
2024-07-10 16:07:33 +0300

    unify dependency version management with version catalog...

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,16 +7,6 @@ pluginManagement {
     mavenLocal()
   }
 
-  plugins {
-    kotlin("jvm").version(extra["kotlin.version"] as String)
-    kotlin("multiplatform").version(extra["kotlin.version"] as String)
-    kotlin("plugin.serialization").version(extra["kotlin.version"] as String)
-    kotlin("android").version(extra["kotlin.version"] as String)
-    id("com.android.base").version(extra["agp.version"] as String)
-    id("com.android.application").version(extra["agp.version"] as String)
-    id("com.android.library").version(extra["agp.version"] as String)
-    id("org.jetbrains.dokka").version("1.9.20")
-  }
 }
 
 rootProject.name = "ksui"
