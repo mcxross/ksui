777ab5aa5092a9ee6eafa0c13cfd019384694234
astinz <28899947+astinz@users.noreply.github.com>
2024-07-12 14:19:04 +0300

    bump build version...

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 group = "xyz.mcxross.ksui"
 
-version = "2.1.0-SNAPSHOT"
+version = "2.1.1-SNAPSHOT"
 
 repositories {
   maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots") }
