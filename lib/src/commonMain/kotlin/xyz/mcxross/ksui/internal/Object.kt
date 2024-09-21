a6750e05596b280e833edede3be0a5bc44d34b5a
astinz <28899947+astinz@users.noreply.github.com>
2024-09-21 16:27:38 +0800

    minor fix...

--- lib/src/commonMain/kotlin/xyz/mcxross/ksui/internal/Object.kt
+++ lib/src/commonMain/kotlin/xyz/mcxross/ksui/internal/Object.kt
@@ -43,12 +43,12 @@ internal suspend fun getObject(
     GetObject(
       GetObject.Variables(
         id = id,
-        showBcs = option.showBcs,
-        showOwner = option.showOwner,
+        //showBcs = option.showBcs,
+        //showOwner = option.showOwner,
         showPreviousTransaction = option.showPreviousTransaction,
-        showContent = option.showContent,
+        //showContent = option.showContent,
         showDisplay = option.showDisplay,
-        showType = option.showType,
+        //showType = option.showType,
         showStorageRebate = option.showStorageRebate,
       )
     )
@@ -84,7 +84,7 @@ internal suspend fun getOwnedObjects(
             showBcs = option.showBcs,
             showContent = option.showContent,
             showDisplay = option.showDisplay,
-            showOwner = option.showOwner,
+            //showOwner = option.showOwner,
             showPreviousTransaction = option.showPreviousTransaction,
             showStorageRebate = option.showStorageRebate,
           )
