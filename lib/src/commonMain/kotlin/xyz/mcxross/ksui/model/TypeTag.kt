9c2e1f81081259b994a44569611665d1a38961a6
astinz <128899947+astinz@users.noreply.github.com>
2025-07-13 08:00:37 +0300

    make type tag usable

--- lib/src/commonMain/kotlin/xyz/mcxross/ksui/model/TypeTag.kt
+++ lib/src/commonMain/kotlin/xyz/mcxross/ksui/model/TypeTag.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,90 +17,107 @@ package xyz.mcxross.ksui.model
 
 import kotlinx.serialization.Serializable
 import xyz.mcxross.ksui.serializer.TypeTagSerializer
-
+/**
+ * A sealed class representing a fully-qualified Move type. This provides a type-safe way to
+ * represent types for transaction building, eliminating the need for raw strings.
+ */
 @Serializable(with = TypeTagSerializer::class)
 sealed class TypeTag {
-  abstract fun asMoveType(): String
+  /** Returns the canonical string representation of the Move type. */
+  abstract override fun toString(): String
 
-  object U8 : TypeTag() {
-    override fun asMoveType(): String = toString()
-
-    override fun toString(): String = "u8"
-  }
-
-  object U16 : TypeTag() {
-    override fun asMoveType(): String = toString()
-
-    override fun toString(): String = "u16"
-  }
-
-  object U32 : TypeTag() {
-    override fun asMoveType(): String = toString()
-
-    override fun toString(): String = "u32"
-  }
-
-  object U64 : TypeTag() {
-    override fun asMoveType(): String = toString()
-
-    override fun toString(): String = "u64"
-  }
-
-  object U128 : TypeTag() {
-    override fun asMoveType(): String = toString()
-
-    override fun toString(): String = "u128"
-  }
-
-  object U256 : TypeTag() {
-    override fun asMoveType(): String = toString()
-
-    override fun toString(): String = "u256"
-  }
-
-  object Bool : TypeTag() {
-    override fun asMoveType(): String = toString()
-
-    override fun toString(): String = "bool"
-  }
-
-  object Address : TypeTag() {
-    override fun asMoveType(): String = toString()
-
-    override fun toString(): String = "address"
-  }
-
-  class Vector : TypeTag() {
-
-    private var of: TypeTag? = null
-
-    fun of(typeTag: TypeTag): Vector {
-      of = typeTag
-      return this
-    }
-
-    override fun asMoveType(): String = toString()
-
-    override fun toString(): String = "vector<$of>"
+  @Serializable
+  data object Bool : TypeTag() {
+    override fun toString() = "bool"
   }
 
   @Serializable
-  data class Struct(
-    val address: AccountAddress,
-    //val module: Identifier,
-    //val name: Identifier,
-    val typeParams: List<TypeTag> = emptyList(),
-  ) : TypeTag() {
-    override fun asMoveType(): String = toString()
+  data object U8 : TypeTag() {
+    override fun toString() = "u8"
+  }
 
-    override fun toString(): String = "struct<$address::${TODO()}::${TODO()}>"
+  @Serializable
+  data object U16 : TypeTag() {
+    override fun toString() = "u16"
+  }
 
-    companion object {
-      fun from(str: String): Struct {
-        val parts = str.split("::")
-        require(parts.size == 3) { "Invalid struct type tag: $str" }
-        return Struct(AccountAddress.fromString(parts[0]), TODO())
+  @Serializable
+  data object U32 : TypeTag() {
+    override fun toString() = "u32"
+  }
+
+  @Serializable
+  data object U64 : TypeTag() {
+    override fun toString() = "u64"
+  }
+
+  @Serializable
+  data object U128 : TypeTag() {
+    override fun toString() = "u128"
+  }
+
+  @Serializable
+  data object U256 : TypeTag() {
+    override fun toString() = "u256"
+  }
+
+  @Serializable
+  data object Address : TypeTag() {
+    override fun toString() = "address"
+  }
+
+  @Serializable
+  data object Signer : TypeTag() {
+    override fun toString() = "signer"
+  }
+
+  /**
+   * Represents a Move `vector<T>` type.
+   *
+   * @param elementType The `TypeTag` of the elements in the vector.
+   */
+  @Serializable
+  data class Vector(val elementType: TypeTag) : TypeTag() {
+    override fun toString() = "vector<${elementType}>"
+  }
+
+  /**
+   * Represents a Move `struct` type.
+   *
+   * @param tag The `StructTag` containing the full identifier of the struct.
+   */
+  @Serializable
+  data class Struct(val tag: StructTag) : TypeTag() {
+    override fun toString() = tag.toString()
+  }
+}
+
+/**
+ * Represents the fully-qualified identifier of a Move struct.
+ *
+ * @param address The address of the package where the struct is defined.
+ * @param module The name of the module where the struct is defined.
+ * @param name The name of the struct.
+ * @param typeParams A list of `TypeTag`s for any generic type parameters.
+ */
+@Serializable
+data class StructTag(
+  val address: String,
+  val module: String,
+  val name: String,
+  val typeParams: List<TypeTag> = emptyList(),
+) {
+  override fun toString(): String {
+    val params =
+      if (typeParams.isNotEmpty()) {
+        "<${typeParams.joinToString(", ")}>"
+      } else {
+        ""
       }
-    }
+    return "$address::$module::$name$params"
+  }
+
+  companion object {
+    val STRING = StructTag("0x1", "string", "String")
   }
 }
