0de467aa645c063ad9fa3f71f919b1bd90e7ddbe
astinz <28899947+astinz@users.noreply.github.com>
2026-04-19 02:08:41 +0300

    Fix gRPC SuiNS reverse lookup test

--- lib/src/grpcTest/kotlin/xyz/mcxross/ksui/grpc/e2e/GrpcSnsTest.kt
+++ lib/src/grpcTest/kotlin/xyz/mcxross/ksui/grpc/e2e/GrpcSnsTest.kt
@@ -1,5 +1,8 @@
 package xyz.mcxross.ksui.grpc.e2e
 
+import io.grpc.Status
+import io.grpc.StatusException
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import xyz.mcxross.ksui.TestResources
@@ -13,14 +16,15 @@ class GrpcSnsTest :
   StringSpec({
     val alice = TestResources.alice
 
-    "Reverse lookup name service names returns successfully on testnet" {
+    "Reverse lookup returns NOT_FOUND when an address has no SuiNS reverse record" {
       val client = SuiGrpcClient.fromConfig(SuiConfig(SuiSettings(network = Network.TESTNET)))
       try {
-        runBlocking {
-          val response = client.reverseLookupName(alice.address)
+        val error =
+          shouldThrow<StatusException> {
+            runBlocking { client.reverseLookupName(alice.address) }
+          }
 
-          response.record.targetAddress?.let { it shouldBe alice.address.toString() }
-        }
+        error.status.code shouldBe Status.Code.NOT_FOUND
       } finally {
         client.close()
       }
