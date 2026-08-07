ba8662c137babab6efb4c153378d5e675743aead
astinz <28899947+astinz@users.noreply.github.com>
2026-08-07 17:51:58 +0300

    chore: remove obsolete sample apps

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 
 rootProject.name = "ksui"
 
-include(":core", ":graphql", ":grpc", ":sample:jvm", ":sample:android")
+include(":core", ":graphql", ":grpc", ":sample:jvm")
 
 project(":core").name = "ksui-core"
 
